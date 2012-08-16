5ba3a114ae73d0432ef11a9faa1a26a4a6e1edfa
Schlaefer <openmail+sourcecode@siezi.com>
2012-08-16 15:15:45 +0200

    test that hash starts with bcrypt prefix

--- Test/Case/Controller/Component/Auth/BcryptAuthenticateTest.php
+++ Test/Case/Controller/Component/Auth/BcryptAuthenticateTest.php
@@ -7,6 +7,8 @@
     public function testPassword() {
       $password = 'Rosinenbrötchen';
       $hash = BcryptAuthenticate::hash($password);
+
+			$this->assertStringStartsWith('$2a$'. BcryptAuthenticate::$cost, $hash);
       $this->assertTrue(BcryptAuthenticate::checkPassword($password, $hash));
 
       $this->assertFalse(BcryptAuthenticate::checkPassword(mt_rand(1, 99999), $hash));
